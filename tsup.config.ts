2b041faf660564266581e2397e5711251555ab79
lucaslos <lucas@sugestly.com>
2025-05-23 15:34:24 -0300

    Update build configuration in package.json, remove composite option from tsconfig.json, and enable dts generation in tsup.config.ts.

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,5 +4,7 @@ export default defineConfig({
   entry: ['src/main.ts', 'src/nodeLogger.ts'],
   outDir: 'dist',
   clean: true,
+  dts: true,
+  tsconfig: './tsconfig.prod.json',
   format: ['esm', 'cjs'],
 });
